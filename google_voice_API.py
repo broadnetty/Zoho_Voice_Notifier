9eb2a9c3aa2a27b6a77a534d91597d2eef3145ae
broadnetty <xupypg88@gmail.com>
2018-10-23 01:00:30 +0300

    First tests - created speaker class (sketch) and adde warning when API isn not connecting. JSON config added

--- google_voice_API.py
+++ google_voice_API.py
@@ -1,36 +1,71 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 from google.cloud import texttospeech
-import os, sys
+import os, sys,platform
+import json
 
-# Instantiates a client
-client = texttospeech.TextToSpeechClient()
 
-ssml='<speak>  Here are <say-as interpret-as="characters">SSML</say-as> samples.  I can pause <break time="3s"/>.  I can play a sound  <audio src="https://www.example.com/MY_MP3_FILE.mp3">didn\'t get your MP3 audio file</audio>.  I can speak in cardinals. Your number is <say-as interpret-as="cardinal">10</say-as>.  Or I can speak in ordinals. You are <say-as interpret-as="ordinal">10</say-as> in line.  Or I can even speak in digits. The digits for ten are <say-as interpret-as="characters">10</say-as>.  I can also substitute phrases, like the <sub alias="World Wide Web Consortium">W3C</sub>.  Finally, I can speak a paragraph with two sentences.  <p><s>This is sentence one.</s><s>This is sentence two.</s></p></speak>'
-# Set the text input to be synthesized
-synthesis_input = texttospeech.types.SynthesisInput(ssml=ssml)#text="You have 3 emails. 1st from Nicolas: How to make my backups work? ")
+class SynthSpeaker():
 
-# Build the voice request, select the language code ("en-US") and the ssml
-# voice gender ("neutral")
-voice = texttospeech.types.VoiceSelectionParams(
-    language_code='en-gb',
-    ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+    #Load config from config.json
+    def __init__(self):
+        config_path="config.json"
 
-# Select the type of audio file you want returned
-audio_config = texttospeech.types.AudioConfig(
-    audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+        #checking for alternate config path
+        if 'ZOHO_SPEAKER_CONFIG_PATH' in os.environ:
+            config_path = os.environ['ZOHO_SPEAKER_CONFIG_PATH']
 
-# Perform the text-to-speech request on the text input with the selected
-# voice parameters and audio file type
-response = client.synthesize_speech(synthesis_input, voice, audio_config)
+        #loading credentials downloaded
+        with open(config_path) as fileconf:
+            self.config = json.load(fileconf)
+            os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = self.config['config']['voice']['auth_json_path']
 
-# The response's audio_content is binary.
-with open('output.mp3', 'wb') as out:
-    # Write the response to the output file.
-    out.write(response.audio_content)
-    print('Audio content written to file "output.mp3"')
+        self.setup('data')
+        return
 
-print("Playing MP3..")
 
-os.system('cmdmp3\cmdmp3win.exe output.mp3')
-print('done.')
+    def download(self, text):
+        try:
+            client = texttospeech.TextToSpeechClient()
+            response = client.synthesize_speech(text, self.voice, self.audio_config)
+        except Exception:
+            self.play('API_error.mp3')
+
+        # The response's audio_content is binary.
+        with open(self.config['config']['voice']['temp_mp3_path'], 'wb') as out:
+            # Write the response to the output file.
+            out.write(response.audio_content)
+            print('Audio content written to file "output.mp3"')
+        return
+
+    def play(self, file_path):
+        print("Playing MP3..")
+
+        os.system(self.config['config']['voice']['mp3player_cmd'] + ' '
+                  + file_path + ' ' +
+                  self.config['config']['voice']['mp3_params'])
+        print('done.')
+        return
+
+    def say(self, text):
+        self.download(texttospeech.types.SynthesisInput(text=text))
+        self.play(self.config['config']['voice']['temp_mp3_path'])
+        return
+
+    def setup(self, data):
+        # Build the voice request, select the language code ("en-US") and the ssml
+        # voice gender ("neutral")
+        self.voice = texttospeech.types.VoiceSelectionParams(
+            language_code='en-ir',
+            ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE)
+
+        # Select the type of audio file you want returned
+        self.audio_config = texttospeech.types.AudioConfig(
+            audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+        return
+
+
+
+speaker = SynthSpeaker()
+
+speaker.say('Hi guys!')
