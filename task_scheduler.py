cc38c811237f2f331749fb896fb180f9b5eab0d7
Mike Topskiy <36136553+broadnetty@users.noreply.github.com>
2018-11-16 15:53:46 +0300

    First tests - created speaker class (sketch) and adde warning when API isn not connecting. JSON config added

--- task_scheduler.py
+++ task_scheduler.py
@@ -8,101 +8,114 @@ import datetime
 from datetime import timedelta
 
 
-def init():
+def init_loger():
     #logger init
     #fileConfig('logging_config.ini')
     logger = logging.getLogger()
-    logging.basicConfig(filename='logs\\' + str(datetime.datetime.today()).replace(':','-') + '.log',
+    filename = 'logs\\' + str(datetime.datetime.today()).replace(':','-') + '.log'
+    logging.basicConfig(filename=filename,
                         filemode='a',
-                        level=logging.DEBUG,
+                        level=logging.INFO,
                         format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s')
-    logging.debug("Process exited %s", "successfully")
+    with open('logs\\' + str(datetime.datetime.today()).replace(':','-') + '.log', 'a') as logfile:
+        logfile.writelines(['=' * 50,'\nStarting a new log...'])
     return logger
 
-#if __name__ == '__main__':
-#    logger = init()
-
-#rs = ZohoWorker()
 
 def kbfunc():
     return msvcrt.getch() if msvcrt.kbhit() else 0
 
+
+#general class for a task. Methods could be overwritten
 class Task:
 
-    def  __init__(self, func, period):
+    def __init__(self,task, period):
         self.id = ''
         self.name = 'Task'
-        self.start_time = datetime.datetime.now()
+        self.next_start_time = datetime.datetime.now()
         self.period_sec = period
-        self.do = self.set_action(func)
+        self.do = self.set_action(task.do)
         return
 
-    def update(self):
-        self.start_time += datetime.timedelta(seconds=self.period_sec)
+    #should be overloaded
+    def setup(self):
+        return
+
+    def update_time(self):
+        self.next_start_time += datetime.timedelta(seconds=self.period_sec)
         return
 
     def set_action(self,func):
         def wraper():
             func()
-            self.update()
+            self.update_time()
         return wraper
 
+#defines the task scheduler. It's better to have one only
 class TaskScheduler:
 
     def __init__(self):
         self.list = list()
         return
 
+    #starts added tasks sequentially (checks if next run time reached then task->do )
     def start_tasks(self):
         while True:
             for task in self.list:
-                if task.start_time < datetime.datetime.now() - datetime.timedelta(seconds=task.period_sec):
+                if task.next_start_time < datetime.datetime.now() - datetime.timedelta(seconds=task.period_sec):
                     task.do()
 
-    def add_task(self, func, period):
-        self.list.append(Task(func, period))
+    def add_task(self, task,period):
+        self.list.append(Task(task,period))
         return
 
 
-config_path="config.json"
+class ZHNotifier():
+    global_notication = list()
+    notificated = list()
 
-#checking for alternate config path
-if 'ZOHO_SPEAKER_CONFIG_PATH' in os.environ:
-    config_path = os.environ['ZOHO_SPEAKER_CONFIG_PATH']
+    def __init__(self):
+        self.config_path = "config.json"
+        # checking for alternate config path
+        if 'ZOHO_SPEAKER_CONFIG_PATH' in os.environ:
+            self.config_path = os.environ['ZOHO_SPEAKER_CONFIG_PATH']
 
-with open(config_path) as fileconf:
-    config = json.load(fileconf)
+        with open(self.config_path) as fileconf:
+            config = json.load(fileconf)
 
-zoj = ZohoWorker(config['config']['zoho'])
+        self.zhworker = ZohoWorker(config['config']['zoho'])
 
-global_notication = list ()
-notificated = list()
 
-def f1():
-    for ticket in zoj.getTicketsByStatus(Status.open):
-        if ticket['id'] not in notificated:
-            global_notication.append(ticket)
-            print(datetime.datetime.now())
-            print(ticket['ticketNumber'] + ' has added to notified\n')
+    def get_tickets(self):
+        for ticket in self.zhworker.getTicketsByStatus(Status.open):
+            if ticket['id'] not in self.notificated:
+                self.global_notication.append(ticket)
+                print(datetime.datetime.now())
+                print(ticket['ticketNumber'] + ' has added to notified\n')
 
-def f2():
-    speaker = SynthSpeaker()
-    for item in global_notication:
-        if item['id'] not in notificated:
-            notificated.append(item['id'])
-            text = 'You\'ve got a new open case number ' \
-            + item['ticketNumber'] \
-            + '. Subject. ' \
-            + item['subject']
-            speaker.say(text)
+    def notify(self):
+        speaker = SynthSpeaker()
+        for item in self.global_notication:
+            if item['id'] not in self.notificated:
+                self.notificated.append(item['id'])
+                text = 'You\'ve got a new open case number ' \
+                + item['ticketNumber'] \
+                + '. Subject. ' \
+                + item['subject']
+                speaker.say(text)
 
-def mf1():
-    f1()
-    f2()
+    def do(self):
+        self.get_tickets()
+        self.notify()
 
-sch = TaskScheduler()
-sch.add_task(mf1,5)
+def main():
+    log = init_loger()
+    log.info('Starting new loop')
 
-print('starting')
-sch.start_tasks()
+    sch = TaskScheduler()
+    sch.add_task(ZHNotifier(),5)
 
+    print('starting')
+    sch.start_tasks()
+
+main()
